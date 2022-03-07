b7e50f6068ce3259840144916f077bd19583a2ec
kundlatsch <gustavo.kundlatsch@gmail.com>
2022-03-06 21:36:59 -0300

    :(

--- src/architecture/armv8/armv8_cpu_syscall.cc
+++ src/architecture/armv8/armv8_cpu_syscall.cc
@@ -13,8 +13,17 @@ void CPU::syscall(void * msg)
         "stp x2, x3, [sp, #-16]  \n"
         //Chamando sycall
         "mov x0, %0 \n"
+        "mov x1, sp \n"  // salvar tmp sp_usr
+        "mrs x2, sp_el0 \n" // carregar sp_svc
+        "msr sp_el1, x2 \n"
+        "msr sp_el0, x1 \n"// salvar sp_usr
 
         "SVC 0x0    \n"
+
+        "mov x1, sp \n"  // salvar tmp sp_usr
+        "mrs x2, sp_el0 \n" // carregar sp_svc
+        "msr sp_el1, x2 \n"
+        "msr sp_el0, x1 \n"// salvar sp_usr
 
         //Retornando contexto
         "ldp x2, x3, [sp, #16]  \n"
