16b053d082ab335b77fa870ad6eb2128c9e94a76
paolabel <pa0la.abel@hotmail.com>
2022-03-06 23:22:33 -0300

    pelo menos começa o epos

--- src/architecture/armv8/armv8_cpu_syscall.cc
+++ src/architecture/armv8/armv8_cpu_syscall.cc
@@ -6,25 +6,9 @@ __BEGIN_SYS
 
 void CPU::syscall(void * msg)
 {
-    ASM(
-        //Salvando contexto
-        "str lr, [sp, #-8]!   \n"
-        "stp x0, x12, [sp, #-16]  \n"
-        //Chamando sycall
-        // "mov x0, %0 \n"
-        // "mov x12, [sp] \n"  // salvar tmp sp_usr
-        // "mrs [sp], [sp_usr] \n" // carregar sp_svc
-        // "msr [sp_usr], x12 \n"// salvar sp_usr
-
-        // "SVC 0x0    \n"
-
-        // "mov x12, [sp] \n"  //salvar tmp sp_svc
-        // "mrs [sp], [sp_usr] \n" //carregar sp_usr
-        // "msr [sp_usr], x12 \n" //salver_sp_svc
-
-        //Retornando contexto
-        "ldp x0, x12, [sp, #16]   \n"
-        "ldr lr, [sp, #8]!   \n"
+      ASM(
+        "mov %0, x0     \n" 
+        "SVC 0x0    \n"
         "" :: "r"(msg)
     );
 }
